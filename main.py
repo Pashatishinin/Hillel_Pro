70016fc7a23c547d5c0e5070e4d75d9093492a5a
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2023-01-07 16:15:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,45 @@ if __name__ == '__main__':
                                               'sclient': 'gws-wiz-serp'}
     assert parse_cookie('search_query=python+vs+javascript') == {'search_query': 'python+vs+javascript'}
     assert parse_cookie('ReturnUrl=%2Fru%2Fcabinet') == {'ReturnUrl': '%2Fru%2Fcabinet'}
+    
+    import urllib
+from urllib.parse import urlparse
+
+
+
+def parse(query: str) -> dict:
+    parser = urlparse(query)
+
+    return dict(urllib.parse.parse_qsl(parser.query))
+
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('https://py.checkio.org/en/mission/just-fizz/') == {}
+    assert parse('https://www.codewars.com/kata/57a0885cbb9944e24c00008e/solutions/python') == {}
+    assert parse('https://translate.google.de/?hl=ru&tab=rT&sl=en&tl=ru&op=translate') == {'hl': 'ru', 'tab': 'rT',
+                                                                                           'sl': 'en', 'tl': 'ru',
+                                                                                           'op': 'translate'}
+    assert parse('https://lms.ithillel.ua/groups/634317c22fa78f4b3a6fd66c/homeworks/63ab49bedd0ec14f08fb1d8f') == {}
+    assert parse('https://ru.wikipedia.org/wiki/Python') == {}
+    assert parse('https://github.com/Pashatishinin/Hillel_Pro') == {}
+    assert parse('https://www.youtube.com/') == {}
+    assert parse('https://itvdn.com/ru/subscriptions') == {}
+    assert parse('https://www.youtube.com/watch?v=vou0HS2EXos') == {"v": "vou0HS2EXos"}
+    assert parse('https://www.youtube.com/watch?v=z5lo2_mMZ3E') == {"v": "z5lo2_mMZ3E"}
+
+
+
+# def parse_cookie(query: str) -> dict:
+#     return {}
+#
+#
+# if __name__ == '__main__':
+#     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+#     assert parse_cookie('') == {}
+#     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+#     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
